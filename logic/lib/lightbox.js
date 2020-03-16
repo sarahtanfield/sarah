fb98fe39d6bda012c9dd64bbf95834c18c01ec0f
Hamish MacDonald <h@kor.nz>
2020-03-17 01:43:10 +1300

    Add progress messing with lightbox next/prev.

--- logic/lib/lightbox.js
+++ logic/lib/lightbox.js
@@ -10,6 +10,9 @@ function Lightbox()
   this.right = null;
   this.black = null;
 
+  itemList = null;
+  itemCurrent = null;
+
   this.install = function(main, prefix)
   {
     this.main = main;
@@ -34,11 +37,11 @@ function Lightbox()
     this.left = document.createElement('div');
     this.left.className = this.prefix + '-left';
     this.imgContainer.appendChild(this.left);
-    this.addEvent(this.left, 'click', function(){ console.log('left'); });
+    this.addEvent(this.left, 'click', this.navLeft);
     this.right = document.createElement('div');
     this.right.className = this.prefix + '-right';
     this.imgContainer.appendChild(this.right);
-    this.addEvent(this.right, 'click', function(){ console.log('right'); });
+    this.addEvent(this.right, 'click', this.navRight);
 
     this.desc = document.createElement('div');
     this.desc.className = this.prefix + '-desc';
@@ -47,9 +50,11 @@ function Lightbox()
     this.main.appendChild(this.container);
   }
 
-  this.load = function(file, desc)
+  this.load = function(file, desc, list)
   {
     this.img.src = file;
+    itemCurrent = file;
+    itemList = list;
 
     if (desc != '' && desc != undefined)
     {
@@ -65,6 +70,22 @@ function Lightbox()
     this.main.style.display = 'block';
     
     document.body.style.overflow='hidden';
+  }
+
+  this.navLeft = function()
+  {
+    console.log('left');
+    console.log(itemCurrent);
+    console.log(itemList);
+    // TODO: find itemCurrent in itemList, load prev
+  }
+
+  this.navRight = function()
+  {
+    console.log('right');
+    console.log(itemCurrent);
+    console.log(itemList);
+    // TODO: find itemCurrent in itemList, load next
   }
 
   this.close = function()
